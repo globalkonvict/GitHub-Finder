4c81bb7b674413dcb34412c2457131f0895be8fd
Sarthak Dwivedi <sarthakwarlock@gmail.com>
2020-05-01 17:31:33 +0530

    edited readme

--- src/context/github/GithubState.js
+++ src/context/github/GithubState.js
@@ -5,6 +5,9 @@ import githubReducer from './GithubReducer';
 import { GET_USER, GET_REPOS, SET_LOADING, SEARCH_USERS } from '../types';
 
 const GithubState = (props) => {
+  const CLIENT_ID = process.env.CLIENT_ID;
+  const CLIENT_SECRET = process.env.CLIENT_SECRET;
+
   const initialState = {
     user: {},
     users: [],
@@ -17,19 +20,21 @@ const GithubState = (props) => {
   const searchUser = async (text) => {
     setLoading();
     const request = await axios.get(
-      `https://api.github.com/search/users?q=${text}`
+      `https://api.github.com/search/users?q=${text}&client_id=${CLIENT_ID}&client_secret=${CLIENT_SECRET}`
     );
     dispatch({ type: SEARCH_USERS, payload: request.data.items });
   };
 
   const getSingleUser = async (username) => {
-    const request = await axios.get(`https://api.github.com/users/${username}`);
+    const request = await axios.get(
+      `https://api.github.com/users/${username}?client_id=${CLIENT_ID}&client_secret=${CLIENT_SECRET}`
+    );
     dispatch({ type: GET_USER, payload: request.data });
   };
 
   const getRepos = async (url) => {
     const repos = await axios.get(
-      `${url}?per_page=5&created:asc?client_id=${process.env.CLIENT_ID}&client_secret=${process.env.CLIENT_SECRET}`
+      `${url}?per_page=5&created:asc?client_id=${CLIENT_ID}&client_secret=${CLIENT_SECRET}`
     );
     const repoList = await repos.data;
     dispatch({ type: GET_REPOS, payload: [...repoList] });
